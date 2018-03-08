2acd5a968c433e0f6364d626ee4d6ff08109ac90
fidofigo <394730097@qq.com>
2018-03-08 14:07:42 +0800

    box相关初始化

--- src/main/java/com/ygg/admin/dao/impl/mybatis/base/BaseDaoImpl.java
+++ src/main/java/com/ygg/admin/dao/impl/mybatis/base/BaseDaoImpl.java
@@ -6,21 +6,11 @@ import org.mybatis.spring.SqlSessionTemplate;
 
 public class BaseDaoImpl
 {
-    
     @Resource
     private SqlSessionTemplate sqlSessionTemplateAdmin;
-
-    @Resource
-    private SqlSessionTemplate sqlSessionTemplateShop;
 
     protected SqlSessionTemplate getSqlSessionAdmin()
     {
         return sqlSessionTemplateAdmin;
     }
-
-    protected SqlSessionTemplate getSqlSessionShop()
-    {
-        return sqlSessionTemplateShop;
-    }
-    
 }
